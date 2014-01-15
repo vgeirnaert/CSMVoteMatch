0484905a0f4e1c9a3a3df37eca009d40bf0a0cce
Valentijn Geirnaert <gsfjohndoe@hotmail.com>
2014-01-15 21:05:57 +0100

    first part of code cleanup

--- website/js/compare.js
+++ website/js/compare.js
@@ -3,32 +3,6 @@
 function TraditionalCompareClass() {
 }
 
-// scoreQuestion takes the answer to a question from person A (you, the user) and compares them to the 
-// answer of personB (the candidate), then returns a fidelity score for this question. The answers
-// also include weights, indicating how important this issue is to each person.
-TraditionalCompareClass.prototype.scoreQuestion = function(answerA, weightA, answerB, weightB) {
-	var score = 0;
-	
-	if(answerA == 0) // if person A (you, the user) doesn't care about this question
-		score = 0; // neutral fidelity, regardless of person B's answer
-	else if(answerA == answerB) // if both are exactly the same 
-		score = 2;	// strong positive fidelity
-	else if( (Math.max(answerA, answerB) == 2 && Math.min(answerA, answerB) == 1 ) || (Math.max(answerA, answerB) == -1 && Math.min(answerA, answerB) == -2 ) ) // if both have 'agree' and 'strongly agree', or both have 'disagree' and 'strongly disagree'
-		score = 1; // weak positive fidelity
-	else {
-		var difference = Math.min(answerA, answerB) - Math.max(answerA, answerB); // get relative distance between answer A and B
-		if(difference >= -2) // if the distance is -2 or less (0 or -1) -- examples: "disagree" vs "agree", "agree" vs "no opinion"
-			score = -1; // weak negative fidelity
-		else // if the distance is more than -2 (-3 or -4) -- examples: "agree" vs "strongly disagree", "strongly disagree" vs "no opinion"
-			score = -2; // strong negative fidelity
-	}
-	
-	var weight = this.getWeight(weightA, weightB, answerA, answerB);
-	
-	// return weighted score
-	return (score * weight) * weightA;
-}
-
 // score questions according to the OKCupid method (more or less)
 // http://www.okcupid.com/help/match-percentages
 // userAnswer has to be a javascript array, even if it only contains one element
@@ -43,47 +17,6 @@ TraditionalCompareClass.prototype.scoreQuestionOKC = function(userAnswerArray, w
 	
 	// if the candidate's answer isn't acceptable, return score 0
 	return 0;
-}
-
-// return the matching score of two weights
-TraditionalCompareClass.prototype.getWeight = function(weightA, weightB, answerA, answerB) {
-	// what do we consider to be the cutoff (inclusive) between important and not important:
-	var important = 1;
-	
-	// the multipliers we return for fidelity increases and decreases
-	var increase = 1.5;
-	var decrease = 0.5;
-	
-	// if both people agree this is important (or of neutral importance)
-	// increase their fidelity (tend away from 0)
-	if(weightA >= important && weightB >= important)
-		return increase;
-		
-	// if both people agree this is not important
-	if(weightA < important && weightB < important) {
-		// if they agree on their answer, increase their fidelity (tend away from 0)
-		if( (answerA > 0 && answerB > 0) || (answerA < 0 && answerB < 0) )
-			return increase;
-		else // decrease their fidelity (tend towards 0)
-			return decrease;
-	}
-	
-	// if the people disagree on this question's importance
-	// decrease their fidelity (tend towards 0)
-	if( Math.max(weightA, weightB) >= important && Math.min(weightA, weightB) < important)
-		return decrease;
-}
-
-// getMaxFidelityScore returns the maximum fidelty score
-// obtainable in a perfect match
-TraditionalCompareClass.prototype.getMaxFidelityScore = function(totalQuestions) {
-	return this.scoreQuestion(2,1,2,1) * totalQuestions;
-}
-
-// getMinFidelityScore returns the lowest fidelty score
-// obtainable in case of the worst possible match
-TraditionalCompareClass.prototype.getMinFidelityScore = function(totalQuestions) {
-	return this.scoreQuestion(2,1,-2,1) * totalQuestions;
 }
 
 // takes an integer number and returns a string
@@ -105,23 +38,7 @@ function matchCandidate(user, comparer, candidateIndex) {
 	var score = 0;
 	var candidate = candidates[candidateIndex];
 	var candidateScore = {classic_score:[], okc_score:[]};
-	candidateScore["candidate"] = candidateIndex;
-	
-	/*for(var i = 0; i < matchQuestions.length; i++) {
-		// first, get the answers and weights from our user and candidate
-		var answerA = user.classic_answers[getQuestionKey(matchQuestions[i])]["answer"];
-		var weightA = user.classic_answers[getQuestionKey(matchQuestions[i])]["weight"];
-		var answerB = candidate.classic_answers[getQuestionKey(matchQuestions[i])]["answer"];
-		var weightB = candidate.classic_answers[getQuestionKey(matchQuestions[i])]["weight"];
-		
-		// then calculate a fidelity score and add it to the tally
-		var questionFidelity = comparer.scoreQuestion(answerA, weightA, answerB, weightB);
-		score += questionFidelity;
-		
-		var questionScore = {fidelity:questionFidelity, weight:comparer.getWeight(weightA, weightB, answerA, answerB)};
-		candidateScore.classic_score[getQuestionKey(matchQuestions[i])] = questionScore;
-	}*/
-	
+	candidateScore["candidate"] = candidateIndex;	
 	var okcScoreU = 0;
 	var maxUserScore = 0;
 	var okcScoreC = 0;
@@ -170,18 +87,14 @@ function matchCandidate(user, comparer, candidateIndex) {
 	scoreOKC = scoreOKC * 100;
 	
 	// turn classic question score into match percentage
-	//score = getPercentage(score, getComparer().getMinFidelityScore(matchQuestions.length), getComparer().getMaxFidelityScore(matchQuestions.length));
 	score = scoreOKC
 	
 	candidateScore["score"] = score;
 	candidateScore["scoreOKC"] = scoreOKC;
 	
-	//var perc_classic_q = (matchQuestions.length / (matchQuestions.length + matchOKCQuestions.length));
-	//var perc_okc_q = (matchOKCQuestions.length / (matchQuestions.length + matchOKCQuestions.length));
-	//candidateScore["combinedScore"] = (score * perc_classic_q) + (scoreOKC * perc_okc_q);
 	candidateScore["combinedScore"] = scoreOKC;
 	
-	//alert(candidate.name + " classic: " + score + ", okc: " + scoreOKC + ", combined: " + candidateScore["combinedScore"]);
+	//console.log(candidate.name + " classic: " + score + ", okc: " + scoreOKC + ", combined: " + candidateScore["combinedScore"]);
 	
 	return candidateScore;
 }
@@ -242,15 +155,10 @@ function printResults(matches) {
 	var html = "";
 	
 	html = html + makeTableHeader(matches);
-	
-	for(var i = 0; i < matchQuestions.length; i++) {
-		html = html + makeRow(matches, i);
-	}
-	
+		
 	// do we have an offset to determine even/uneven rows?
-	evenOffset = matchQuestions.length % 2;
 	for(var i = 0; i < matchOKCQuestions.length; i++) {
-		html = html + makeOKCRow(matches, i, evenOffset);
+		html = html + makeOKCRow(matches, i, 0);
 	}
 	
 	html = html + makeTableFooter(matches);
@@ -265,33 +173,12 @@ function refreshComments() {
 	Opentip.findElements();
 }
 
-function makeRow(matches, row) {
-	
-	var html = "<tr class=\"" + getEvenRow(row) + "\">";
-	var tclass = "";
-	var question = matchQuestions[row];
-	
-	html = html + makeQuestion(row, language, false);
-	
-	for(var i = 0; i < matches.length; i++) {
-		tclass = getClass(i);
-		
-		background = getBackground( ((matches[i]).classic_score[getQuestionKey(question)])["fidelity"], ((candidates[(matches[i])["candidate"]]).classic_answers[getQuestionKey(question)])["comment"] );
-		
-		html = html + "<td class=\"answer " + tclass + " " + background + "\">" + getCellContents( ((candidates[(matches[i])["candidate"]]).classic_answers[getQuestionKey(question)])["answer"], ((candidates[(matches[i])["candidate"]]).classic_answers[getQuestionKey(question)])["weight"], ((candidates[(matches[i])["candidate"]]).classic_answers[getQuestionKey(question)])["comment"], (candidates[(matches[i])["candidate"]])["name"]  ) + "</td>";
-	}
-	
-	html = html + "</tr>";
-	
-	return html;
-}
-
 function makeOKCRow(matches, row, offset) {
 	var html = "<tr class=\"" + getEvenRow(row + offset) + "\">";
 	var tclass = "";
 	var question = okcQuestionIds[matchOKCQuestions[row]];
 	
-	html = html + makeQuestion(row, language, true);
+	html = html + makeQuestion(row, language);
 	
 	for(var i = 0; i < matches.length; i++) {
 		tclass = getClass(i);
@@ -314,16 +201,12 @@ function makeOKCRow(matches, row, offset) {
 	return html;
 }
 
-function makeQuestion(qIndex, lIndex, isOKC) {
+function makeQuestion(qIndex, lIndex) {
 	var html = "<td class=\"question\">";
+
+	html = html + "<label><input type=\"checkbox\" value=\"" + matchOKCQuestions[qIndex] + "\" name=\"okc\" /> ";
+	html = html + okc_questions[matchOKCQuestions[qIndex]].question[lIndex] + "</label></td>";
 	
-	if(isOKC) {
-		html = html + "<label><input type=\"checkbox\" value=\"" + matchOKCQuestions[qIndex] + "\" name=\"okc\" /> ";
-		html = html + okc_questions[matchOKCQuestions[qIndex]].question[lIndex] + "</label></td>";
-	} else {
-		html = html + "<input type=\"checkbox\" value=\"" + matchQuestions[qIndex] + "\" name=\"q\" /> ";
-		html = html + questions[matchQuestions[qIndex]][lIndex] + "</td>";
-	}
 	return html;
 }
 
@@ -370,26 +253,6 @@ function getClass(column) {
 	return c;
 }
 
-function getBackground(fidelity, comment) {
-	var html = "";
-	
-	if(fidelity <= -2)
-		html = "verybad";
-	else if(fidelity < 0)
-		html = "bad";
-	else if(fidelity == 0)
-		html = "neutral";
-	else if(fidelity >= 2)
-		html = "verygood";
-	else if(fidelity > 0)
-		html = "good";
-		
-	if(comment != "")
-		html += " comment";
-		
-	return html;
-}
-
 function getBackgroundOKC(userScore, maxUserScore, candidateScore, maxCandidateScore, comment) {
 
 	var html = "";
@@ -414,38 +277,6 @@ function getBackgroundOKC(userScore, maxUserScore, candidateScore, maxCandidateS
 		else
 			return html + "good";
 	}
-}
-
-function getCellContents(vote, weight, comment, name) {
-	var html = "";
-	
-	if(comment != "")
-		html += '<div data-ot="' + comment + '" data-ot-title="' + name + '" data-ot-containInViewport="true" data-ot-tip-joint="bottom left" data-ot-fixed="true" data-ot-target="true" data-ot-close-button-radius="11" data-ot-close-button-cross-size="10" data-ot-close-button-cross-line-width="3" >';
-		
-	switch(vote) {
-		case -2:
-			html += '<img src="img/doublethumbsdown.png" title="Strongly disagree" />';
-			break;
-		case -1:
-			html += '<img src="img/thumbsdown.png" title="Disagree" />';
-			break;
-		case 0:
-			html += '<img src="img/noopinion.png" title="No opinion" />';
-			break;
-		case 1:
-			html += '<img src="img/thumbsup.png" title="Agree" />';
-			break;
-		case 2:
-			html += '<img src="img/doublethumbsup.png"  title="Strongly agree"/>';
-			break;
-	}
-	
-	if(weight > 1)
-		html = html + '<img src="img/comment.png" title="" />';
-		
-	html = html + '</div>';
-		
-	return html;
 }
 
 function getOKCCellContents(questionId, optionId, match, c_index) {
@@ -593,18 +424,12 @@ function toggleButtonPanel(panel) {
 // Next are the functions dealing with checking
 // and unchecking questions and candidates
 //===============================================
-function resetMatchQuestions() {
-	for(var i = 0; i < questions.length; i++)
-		matchQuestions[i] = i;
-}
-
 function resetOkcQuestions() {
 	for(var i = 0; i < okc_questions.length; i++)
 		matchOKCQuestions[i] = i;
 }
 
 function resetQuestions() {
-	resetMatchQuestions();
 	resetOkcQuestions();
 		
 	printNewGrid();
@@ -767,7 +592,6 @@ function toggleUser() {
 }
 
 // Entry point of our script!
-resetMatchQuestions();
 resetOkcQuestions();
 resetMatchCandidates();
 printNewGrid();
